974e00671d83441f803d46959648d7c04ef7eb17
alieffebrina <alief.febrina@gmail.com>
2020-08-09 21:04:15 +0700

    nota

--- application/controllers/C_Transaksi.php
+++ application/controllers/C_Transaksi.php
@@ -116,35 +116,130 @@ class C_Transaksi extends CI_Controller{
          
     }
 
-    function cetak($ida){
+    function cetak($ida)
+    {
+        $this->load->view('transaksi/cetak'); 
+    }
+
+    function cetak1($ida){
         $pdf = new FPDF('L','mm',array('148', '210'));
         // membuat halaman baru
         $pdf->AddPage();
         // setting jenis font yang akan digunakan
-        $pdf->SetFont('Arial','',10,'C');
+        $pdf->SetFont('Arial','',7,'C');
         // mencetak string       
 
         $pdf->Image('assets/images/kopa.jpg',10,5,190,15);
-        $pdf->Cell(0,15,'',0,1);
-        $pdf->Cell(20,7,'No Resi',1,0,'C');
-        $pdf->Cell(30,7,'Asal',1,0,'C');
-        $pdf->Cell(30,7,'Tujuan',1,0,'C');
-        $pdf->Cell(30,7,'Jumlah Barang',1,0,'C');
-        $pdf->Cell(25,7,'Berat',1,0,'C');
-        $pdf->Cell(25,7,'Berat Volume',1,0,'C');
-        $pdf->Cell(30,7,'Harga/Kg',1,1,'C');
-        $pdf->SetFont('Arial','',11,'C');
+        $pdf->Cell(0,13,'',0,1);
+        $pdf->Cell(20,5,'No Resi',1,0,'C');
+        $pdf->Cell(30,5,'Asal',1,0,'C');
+        $pdf->Cell(30,5,'Tujuan',1,0,'C');
+        $pdf->Cell(30,5,'Jumlah Barang',1,0,'C');
+        $pdf->Cell(25,5,'Berat',1,0,'C');
+        $pdf->Cell(25,5,'Berat Volume',1,0,'C');
+        $pdf->Cell(30,5,'Harga/Kg',1,1,'C');
 
         $data = $this->M_transaksi->getspek($ida);
         foreach ($data as $key ) {
-            $pdf->Cell(20,7,$key->noresi,1,0,'C');
-            $pdf->Cell(30,7,$key->asal,1,0,'C');
-            $pdf->Cell(30,7,'Tujuan',1,0,'C');
-            $pdf->Cell(30,7,'Jumlah Barang',1,0,'C');
-            $pdf->Cell(25,7,'Berat',1,0,'C');
-            $pdf->Cell(25,7,'Berat Volume',1,0,'C');
-            $pdf->Cell(30,7,'Harga/Kg',1,1,'C');
+            $pdf->Cell(20,5,$key->noresi,1,0,'C');
+            $pdf->Cell(30,5,$key->asal,1,0,'C');
+            $pdf->Cell(30,5,$key->tujuan,1,0,'C');
+            $pdf->Cell(30,5,$key->jumlahbarang,1,0,'C');
+            $pdf->Cell(25,5,$key->berat,1,0,'C');
+            $pdf->Cell(25,5,$key->beratvolume,1,0,'C');
+            $pdf->Cell(30,5,'Rp.'.number_format($key->harga),1,1,'C');
+            $pdf->Cell(50,5,'Nama Pengirim : '.$key->namapengirim,1,0);
+            $pdf->Cell(50,5,'Nama Penerima : '.$key->namapenerima,1,0);
+            $pdf->Cell(90,5,'Jenis Barang',1,1,'C');
+            $pdf->Cell(50,5,'NIK : '.$key->nikpengirim,1,0);
+            // $pdf->Cell(50,21,'Alamat Penerima : '.$key->alamatpenerima,'LR');
+            $pdf->cell(50,5,'Alamat Penerima : ','LR');
+            $pdf->Cell(90,5,$key->jenisbarang,1,1,'C');
+            $pdf->cell(50,5,'Alamat Pengirim : ','LR');
+            // $pdf->cell(50,10,$key->alamatpenerima,'LR');
+            $pdf->MultiCell(50, 3, $key->alamatpenerima, 'LR');
+            $pdf->MultiCell(50,3,$key->alamatpengirim,'LR');
+            $pdf->ln();
+            $pdf->Cell(90,5,'INFORMASI JENIS KIRIMAN & LAYANAN',1,1,'C');
+            $pdf->cell(50,5,'','LR');
+            if ($key->jeniskiriman == 'dokumen'){
+                $pdf->Cell(7,5,'v',1,0,'C');
+            } else {
+                $pdf->Cell(7,5,'',1,0,'C');
+            }
+            $pdf->Cell(20,5,'Dokumen',1,0,'C');
+            if ($key->jeniskiriman == 'paket'){
+                $pdf->Cell(7,5,'v',1,0,'C');
+            } else {
+                $pdf->Cell(7,5,'',1,0,'C');
+            }
+            $pdf->Cell(15,5,'Paket',1,0,'C');
+            $pdf->Cell(41,5,$key->jenismuatan,1,1,'C');
+            $pdf->Cell(25,5,'Tlp. Pengirim',1,0,'C');
+            $pdf->Cell(25,5,'Email Pengirim',1,0,'C');
+            $pdf->Cell(25,5,'Tlp. Penerima',1,0,'C');
+            $pdf->Cell(25,5,'Email Penerima',1,0,'C');
+            $pdf->Cell(90,20,'',1,0,'C');
+            $pdf->Image('assets/images/ket.jpg',111,53,75,17);
+            $pdf->Cell(91,5,'',0,1,'C');
+            $pdf->Cell(25,5,$key->tlppengirim,1,0,'C');
+            $pdf->Cell(25,5,$key->emailpengirim,1,0,'C');
+            $pdf->Cell(25,5,$key->tlppenerima,1,0,'C');
+            $pdf->Cell(25,5,$key->emailpenerima,1,1,'C');
+            $pdf->Cell(100,5,'Keterangan :'.$key->ket,1,1,'');
+            $pdf->Cell(25,5,'Ttd/Stempel','LR');
+            $pdf->Cell(25,5,'Ttd/Stempel','LR');
+            $pdf->Cell(25,5,'Ttd/Stempel','LR');
+            $pdf->Cell(25,5,'Nama Penerima',1,1);
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(90,5,'INFORMASI RINCIAN BIAYA',1,1);
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(40,5,'Biaya Kirim',1,0);
+            $pdf->Cell(50,5,'Rp.'.number_format($key->subharga),1,1);
+            $pdf->Cell(25,5,'Pengirim',1,0);
+            $pdf->Cell(25,5,'Petugas',1,0);
+            $pdf->Cell(25,5,'Penerima',1,0);
+            $pdf->Cell(25,5,'No. Handphone',1,0);
+            $pdf->Cell(40,5,'Biaya Packing',1,0);
+            $pdf->Cell(50,5,'Rp.'.number_format($key->biayapacking),1,1);
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(40,5,'Asuransi',1,0);
+            $pdf->Cell(50,5,'Rp.'.number_format($key->asuransi),1,1);
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(25,5,'','LR');
+            $pdf->Cell(40,5,'PPN 1%',1,0);
+            $pdf->Cell(50,5,'Rp.'.number_format($key->ppn),1,1);
+            $pdf->Cell(25,6,'Tgl Transaksi',1,0);
+            $pdf->Cell(25,6,'',1,0);
+            $pdf->Cell(25,6,'Tgl Barang Diterima',1,0);
+            $pdf->Cell(25,6,'Jam Barang Diterima',1,0);
+            $pdf->SetFont('Arial','',10,'C');
+            $pdf->Cell(40,6,'Total Biaya',1,0);
+            $pdf->Cell(50,6,'Rp.'.number_format($key->total),1,1);
+            $pdf->SetFont('Arial','',7,'C');
+            $pdf->Cell(50,5,'Putih : Pengirim / Cash','LR');
+            $pdf->Cell(50,5,'Kuning : Arsip','LR');
+            $pdf->Cell(50,5,'Biru : Tempel Di Paket','LR');
+            $pdf->SetFont('Arial','',8,'C');
+            $pdf->Cell(40,5,'Admin','LR',1);
+            $pdf->Cell(50,10,'','LR');
+            $pdf->Cell(50,10,'','LR');
+            $pdf->Cell(50,10,'','LR');
+            $pdf->Cell(40,10,'','LR',1);
+            $pdf->Cell(190,0,'',1,1);
         }
+        
         // $pdf->AutoPrint(true);
         $pdf->Output();
     }
